c2779af5b2a1d1f9bdcfcb6ecabd12d66246642d
David1456 <balaji.kka@gmail.com>
2019-12-26 02:12:12 +0530

    changed routing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,32 +4,32 @@ import {Link} from "react-router-dom"
 const Navbar = () => {
     return ( 
         <div>
-         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
       
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <Link className="nav-link" to="/">Movies <span className="sr-only">(current)</span></Link>
+            <Link className="nav-link text-danger" to="/">Movies <span className="sr-only">(current)</span></Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" target="_blank" to="/Reviews">Reviews</Link>
+            <Link className="nav-link text-danger"target="_blank" to="/Reviews">Reviews</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link disabled" target="_blank" to="/signup">SignUp</Link>
+            <Link className="nav-link text-danger" target="_blank" to="/signup">SignUp</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link disabled"  to="/Signin">SignIn</Link>
+            <Link className="nav-link text-danger"  to="/Signin">SignIn</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link disabled" target="_blank" to="/About">About</Link>
+            <Link className="nav-link text-danger" target="_blank" to="/About">About</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link disabled" target="_blank" to="/Contact">Contact</Link>
+            <Link className="nav-link text-danger" target="_blank" to="/Contact">Contact</Link>
           </li>
         </ul>
          <form class="form-inline mt-2 mt-md-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <input class="form-control mr-sm-2 " type="text" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
         </form>
         </div>
         </nav>
